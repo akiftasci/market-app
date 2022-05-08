64e954419f48c5823f033902a634b589a50a5c78
akiftascidev <akif@lincsafe.com>
2022-05-08 09:58:53 +0200

    Create entity

--- src/main/java/com/ing/appleMarket/entity/AppleBag.java
+++ src/main/java/com/ing/appleMarket/entity/AppleBag.java
@@ -1,4 +1,39 @@
 package com.ing.appleMarket.entity;
 
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "appleBag")
 public class AppleBag {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    private String id;
+
+    @Column(name = "amount")
+    private int amount;
+
+    @Column(name = "supplier")
+    private String supplier;
+
+    @Column(name = "datetime", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime created;
+
+    @Column(name = "price")
+    private int price;
+
 }
