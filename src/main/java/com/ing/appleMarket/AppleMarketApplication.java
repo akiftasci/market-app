b61bd1de9ce16b4d33484650c527677d71fc4dcd
akiftascidev <akif@lincsafe.com>
2022-05-08 09:03:28 +0200

    Initial commit

---
+++ src/main/java/com/ing/appleMarket/AppleMarketApplication.java
@@ -0,0 +1,13 @@
+package com.ing.appleMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ing/appleMarket/AppleMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.appleMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
