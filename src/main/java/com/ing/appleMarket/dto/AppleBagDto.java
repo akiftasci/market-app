c6ebff6fbc170f3a70955288085124dbb677f053
akiftascidev <akif@lincsafe.com>
2022-05-08 09:58:36 +0200

    Add appleBagDto

--- src/main/java/com/ing/appleMarket/dto/AppleBagDto.java
+++ src/main/java/com/ing/appleMarket/dto/AppleBagDto.java
@@ -1,2 +1,14 @@
-package com.ing.appleMarket.dto;public class AppleBagDto {
+package com.ing.appleMarket.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AppleBagDto {
+    private String id;
+    private int amount;
+    private String supplier;
+    private String date;
+    private int price;
 }
