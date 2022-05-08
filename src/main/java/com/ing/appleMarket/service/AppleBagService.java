56226fad9b524ca8683820feb76b75ff950a8e8f
akiftascidev <akif@lincsafe.com>
2022-05-08 09:59:07 +0200

    Create service layer

--- src/main/java/com/ing/appleMarket/service/AppleBagService.java
+++ src/main/java/com/ing/appleMarket/service/AppleBagService.java
@@ -1,2 +1,17 @@
-package com.ing.appleMarket.service;public class AppleBagService {
+package com.ing.appleMarket.service;
+
+import com.ing.appleMarket.entity.AppleBag;
+import com.ing.appleMarket.repository.AppleMarketRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppleBagService {
+    private final AppleMarketRepository appleMarketRepository;
+
+    public AppleBagService(AppleMarketRepository appleMarketRepository){
+        this.appleMarketRepository = appleMarketRepository;
+    }
+    public AppleBag persistData(final AppleBag appleBag){
+        return appleMarketRepository.save(appleBag);
+    }
 }
