372700f0131cba2c0e93aad4af5b6eea0aaf1ecb
akiftascidev <akif@lincsafe.com>
2022-05-09 15:23:42 +0200

    Add supplier validator

--- src/main/java/com/ing/appleMarket/validator/Supplier.java
+++ src/main/java/com/ing/appleMarket/validator/Supplier.java
@@ -1,4 +1,19 @@
 package com.ing.appleMarket.validator;
 
-public class Supplier {
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = SupplierValidator.class)
+@Documented
+public @interface Supplier {
+    String message() default "Supplier not valid";
+    Class<?>[] groups() default{};
+    Class<? extends Payload>[] payload() default{};
+
 }
--- src/main/java/com/ing/appleMarket/validator/SupplierValidator.java
+++ src/main/java/com/ing/appleMarket/validator/SupplierValidator.java
@@ -1,4 +1,15 @@
 package com.ing.appleMarket.validator;
 
-public class SupplierValidator {
+import java.util.Arrays;
+import java.util.List;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class SupplierValidator implements ConstraintValidator<Supplier, String> {
+    List<String> suppliers = Arrays.asList("Pink Lady", "Royal Gala", "Kanzi Apple", "Elstar Apples");
+
+    @Override
+    public boolean isValid(final String s, final ConstraintValidatorContext context) {
+        return suppliers.contains(s);
+    }
 }
